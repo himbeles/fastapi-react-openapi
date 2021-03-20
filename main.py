ee6b7515dd0e162cd1d1fa6f0a2d4c1481fe3a08
Luis Riegger <lri@me.com>
2021-03-20 16:56:37 +0100

    add template code

---
+++ main.py
@@ -0,0 +1,36 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+
+import json
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+
+# generate OpenAPI JSON file
+
+def use_route_names_as_operation_ids(app: FastAPI) -> None:
+    """
+    Simplify operation IDs so that generated API clients have simpler function
+    names.
+    Should be called only after all routes have been added.
+    """
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            route.operation_id = route.name
+
+
+use_route_names_as_operation_ids(app)
+with open("./openapi.json", "w") as fp:
+    json.dump(app.openapi(), fp=fp)
